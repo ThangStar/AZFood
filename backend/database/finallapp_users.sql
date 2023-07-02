4ecb41cceebe01e5d8beaa8c682d7e1766914a25
FpolyQuocHuy <102716105+FpolyQuocHuy@users.noreply.github.com>
2023-07-02 17:44:26 +0700

    upload package

--- backend/database/finallapp_users.sql
+++ backend/database/finallapp_users.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `finallapp` /*!40100 DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `finallapp`;
 -- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
 -- Host: localhost    Database: finallapp

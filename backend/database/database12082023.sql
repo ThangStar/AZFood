503ccc4ce99a9d63cde85badbc41bf2322bc65bd
FpolyQuocHuy <102716105+FpolyQuocHuy@users.noreply.github.com>
2023-08-12 12:29:49 +0700

    updateDB
    updateDB

--- backend/database/database12082023.sql
+++ backend/database/database12082023.sql
@@ -16,31 +16,6 @@ USE `finallapp`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `statusProduct`
---
-
-DROP TABLE IF EXISTS `statusProduct`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `statusProduct` (
-  `id` int NOT NULL,
-  `name` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `statusProduct`
---
-
-LOCK TABLES `statusProduct` WRITE;
-/*!40000 ALTER TABLE `statusProduct` DISABLE KEYS */;
-INSERT INTO `statusProduct` VALUES (1,'Đang chờ'),(2,'Đang bận'),(3,'Trống');
-/*!40000 ALTER TABLE `statusProduct` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -51,4 +26,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-08-12 12:16:35
+-- Dump completed on 2023-08-12 12:27:51
